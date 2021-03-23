81549b31e6405fe18ec2e24dfb5bcad73ac25966
samroodAli <devsamrood@gmail.com>
2021-03-23 14:24:43 +0530

    implement my_each_with_index in Enumerable module

--- enumerables.rb
+++ enumerables.rb
@@ -1,13 +1,20 @@
-module enumerable
-    def my_each
-       
-         if block_given?
-          for i in self
-           yield i
-        
-          end
-        end
-        to_enum
+module Enumerable
+  def my_each
+    if block_given?
+      for i in self
+        yield i
       end
-end
+    end
+    to_enum
+  end
 
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    i = 0
+    my_each do |el|
+      yield el, i
+      i += 1
+    end
+  end
+end
