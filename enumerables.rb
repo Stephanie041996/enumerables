955020b22090162e13321fe7cbb92cc08302af25
samroodAli <devsamrood@gmail.com>
2021-03-23 18:49:32 +0530

    implement my_inject enumerable

--- enumerables.rb
+++ enumerables.rb
@@ -12,8 +12,8 @@ module Enumerable
     return to_enum unless block_given?
 
     i = 0
-    my_each do |el|
-      yield el, i
+    my_each do |ele|
+      yield ele, i
       i += 1
     end
   end
@@ -22,54 +22,52 @@ module Enumerable
     new_arr = []
     return to_enum unless block_given?
 
-    my_each do |el|
-      new_arr << el if yeild el
+    my_each do |ele|
+      new_arr << ele if yield ele
     end
     new_arr
   end
 
   def my_all?(para = nil)
     if block_given?
-     my_each {|ele| return false unless yield el}
+      my_each { |ele| return false unless yield ele }
     elsif para
-      my_each {|ele| return false unless match?(ele, para)}
+      my_each { |ele| return false unless match?(ele, para) }
     else
-      my_each {|ele| return false unless el}
+      my_each { |ele| return false unless ele }
     end
     true
   end
 
   def my_any?(para = nil)
     if block_given?
-        my_each {|ele| return true if yield ele}
+      my_each { |ele| return true if yield ele }
     elsif para
-      my_each {|ele| return true if match?(ele, para)}
+      my_each { |ele| return true if match?(ele, para) }
     else
-      my_each {|ele| return true if ele}
+      my_each { |ele| return true if ele }
     end
     false
   end
 
   def my_none?(para = nil)
     if block_given?
-     my_each {|ele| return false if yield el}
+      my_each { |ele| return false if yield ele }
     elsif para
-      my_each {|ele| return false if match?(ele, para)}
+      my_each { |ele| return false if match?(ele, para) }
     else
-      my_each {|ele| return false if el}
+      my_each { |ele| return false if ele }
     end
     true
   end
 
-
   def my_count(para = nil)
-    count = 0
     if block_given?
-        my_each {|ele| count +=1 if yield ele}
+      my_each { |ele| count += 1 if yield ele }
     elsif para
-      my_each {|ele| count+=1 if match?(ele, para)}
+      my_each { |ele| count += 1 if match?(ele, para) }
     else
-      my_each {|ele| count+=1 if ele}
+      length
     end
     count
   end
@@ -78,10 +76,35 @@ module Enumerable
     new_arr = []
     return to_enum unless block_given?
 
-    my_each do |el|
-      new_arr << yeild el
+    my_each do |ele|
+      new_arr << yeild(ele)
     end
     new_arr
+  end
+
+  def my_inject(para1 = nil, para2 = nil)
+    array = self.to_a
+    if block_given?
+      acc = para1 ? para1 : array[0]
+      i = para1 ? 0 : 1
+      (i...array.length).each do |idx|
+        acc = yield(acc , array[ idx])
+      end
+    elsif para1 && para2
+      acc = para1 ? para1 : array[0]
+      i = para1 ? 0 : 1
+      (i...array.length).each do |idx|
+         acc = acc.send(para2,array[idx])
+      end
+    elsif para1
+      acc = array[0]
+      (1...array.length).each do |idx|
+        acc = acc.send(para1,array[idx])
+      end
+    else
+      raise LocalJump
+    end
+    acc
   end
 
   # pattern matching function
@@ -95,7 +118,5 @@ module Enumerable
       ele == para
     end
   end
-
   # module end
 end
-
