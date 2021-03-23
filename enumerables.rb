cc6df854807a271f37f72fe60b4d9a8d87680578
samroodAli <devsamrood@gmail.com>
2021-03-23 19:25:30 +0530

    modify my map to take in both proc and bloc

--- enumerables.rb
+++ enumerables.rb
@@ -1,11 +1,8 @@
 module Enumerable
   def my_each
-    if block_given?
-      for i in self
-        yield i
-      end
-    end
-    to_enum
+    return to_enum unless block_given?
+
+    length.times { |i| yield(self[i]) }
   end
 
   def my_each_with_index
@@ -22,9 +19,7 @@ module Enumerable
     new_arr = []
     return to_enum unless block_given?
 
-    my_each do |ele|
-      new_arr << ele if yield ele
-    end
+    my_each { |ele| new_arr << ele if yield ele }
     new_arr
   end
 
@@ -62,6 +57,7 @@ module Enumerable
   end
 
   def my_count(para = nil)
+    count = 0
     if block_given?
       my_each { |ele| count += 1 if yield ele }
     elsif para
@@ -72,49 +68,37 @@ module Enumerable
     count
   end
 
-  def my_map(proc= nil)
+  def my_map(proc = nil)
     new_arr = []
-    
-    return to_enum unless block_given?
-    if block_given?
-    my_each do |ele|
-      new_arr << yeild(ele)
-     end
-    elsif proc
-      my_each do |ele|
-        new_arr << proc.call(ele)
-      end
-    end
+    if proc
+      my_each { |ele| new_arr << proc.call(ele) }
+    elsif block_given?
+      my_each { |ele| new_arr << yield(ele) }
+    else
+      return to_enum unless block_given?
     end
     new_arr
   end
 
   def my_inject(para1 = nil, para2 = nil)
-    array = self.to_a
+    array = to_a
     if block_given?
-      acc = para1 ? para1 : array[0]
+      acc = para1 || array[0]
       i = para1 ? 0 : 1
-      (i...array.length).each do |idx|
-        acc = yield(acc , array[ idx])
-      end
+      (i...array.length).each { |idx| acc = yield(acc, array[idx]) }
     elsif para1 && para2
-      acc = para1 ? para1 : array[0]
+      acc = para1 || array[0]
       i = para1 ? 0 : 1
-      (i...array.length).each do |idx|
-         acc = acc.send(para2,array[idx])
-      end
+      (i...array.length).each { |idx| acc = acc.send(para2, array[idx]) }
     elsif para1
       acc = array[0]
-      (1...array.length).each do |idx|
-        acc = acc.send(para1,array[idx])
-      end
+      (1...array.length).each { |idx| acc = acc.send(para1, array[idx]) }
     else
       raise LocalJump
     end
     acc
   end
 
-  # pattern matching function
   def match?(ele, para)
     case para
     when Regexp
@@ -125,5 +109,20 @@ module Enumerable
       ele == para
     end
   end
-  # module end
 end
+
+
+
+# # Sum some numbers
+# p (5..10).my_inject(:+)                             #=> 45
+#  # Same using a block and my_inject
+# p (5..10).my_inject { |sum, n| sum + n }            #=> 45
+#  # Multiply some numbers
+# p (5..10).my_inject(1, :*)                          #=> 151200
+#  # Same using a block
+# p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
+# # find the longest word
+# longest = %w{ cat sheep bear }.my_inject do |memo, word|
+#     memo.length > word.length ? memo : word
+# end
+# p longest
