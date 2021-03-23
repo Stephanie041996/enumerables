3ff221b60a8960653e91df5d3632b7b23a64ace3
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-03-23 16:23:14 +0200

    Remove comments

--- enumerables.rb
+++ enumerables.rb
@@ -110,19 +110,3 @@ module Enumerable
     end
   end
 end
-
-
-
-# # Sum some numbers
-# p (5..10).my_inject(:+)                             #=> 45
-#  # Same using a block and my_inject
-# p (5..10).my_inject { |sum, n| sum + n }            #=> 45
-#  # Multiply some numbers
-# p (5..10).my_inject(1, :*)                          #=> 151200
-#  # Same using a block
-# p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
-# # find the longest word
-# longest = %w{ cat sheep bear }.my_inject do |memo, word|
-#     memo.length > word.length ? memo : word
-# end
-# p longest
