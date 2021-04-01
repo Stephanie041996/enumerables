e85143925107d76ca4c2ba507a2a8061c0964088
samroodAli <devsamrood@gmail.com>
2021-04-01 13:53:51 +0530

    my each when block is given

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,5 +1,12 @@
 require "enumerables"
 
 describe "Enumerable" do
-  
+  describe "#my_each" do
+    context "when no block is given" do
+      it "should return Enumerable" do
+        array = [1,2,3,4,5]
+        expect(array.my_each).to be_a(Enumerable)
+      end
+    end
+  end
 end
