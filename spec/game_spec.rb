de6fc0b618b86f90f4d61c763253fc468ca28902
shoirata <shoira.shakirovna@gmail.com>
2021-04-13 17:07:20 +0500

    my count test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -68,100 +68,102 @@ end
 
 
 describe 'my_all' do
-describe 'when block is given' do
+     describe 'when block is given' do
+          it'should return true if all elements meet the condition' do
+               arr = [ 1,1,1]
+               result = arr.my_all?{|ele| ele == 1}
+               expect(result).to eq(true)
+          end
+          it'should return false if all elements do not meet the condition' do
+               arr = [ 1,1,2]
+               result = arr.my_all?{|ele| ele == 1}
+               expect(result).to eq(false)
+          end
+     end
+     describe 'when parameter is  given'do
+          it 'should return true' do
+          arr = [1,1,1]
+          expect(arr.my_all?(Integer)).to be true
+          end
 
-     it'should return true if all elements meet the condition' do
-           arr = [ 1,1,1]
-           result = arr.my_all?{|ele| ele == 1}
-           expect(result).to eq(true)
+          it 'should return false' do
+               arr = [1,1,1,"hie"]
+               expect(arr.my_all?(Integer)).to be false
+          end
+          it 'should return true if empty' do
+          expect([].my_all?).to be true
+          end
      end
 
-     it'should return false if all elements do not meet the condition' do
-          arr = [ 1,1,2]
-          result = arr.my_all?{|ele| ele == 1}
-          expect(result).to eq(false)
-    end
-end
-describe 'when parametor is  given'do
+     describe 'when parameter is not given'do
+          it 'should return true' do
+               arr = [1,1,1]
+               expect(arr.my_all?()).to be true
+          end
+          it 'should return false' do
+          arr = [1,1,1,false]
+          expect(arr.my_all?()).to be false
 
-it 'should return true' do
- arr = [1,1,1]
- expect(arr.my_all?(Integer)).to be true
-
-end
-
-it 'should return false' do
-     arr = [1,1,1,"hie"]
-     expect(arr.my_all?(Integer)).to be false
-    
-    end
-end
-describe 'when parametor is not given'do
-it 'should return true' do
-     arr = [1,1,1]
-     expect(arr.my_all?()).to be true
-    end
-it 'should return false' do
- arr = [1,1,1,false]
- expect(arr.my_all?()).to be false
+          end
+     end
 
 end
-end
 
-end
+
+
 
 describe "my_any?" do
-describe ' when block is given'do
-it'should return true if any parametor meets the conditon'do
-arr = [1,2,3,4,5]
- expect(arr.my_any?{|ele| ele > 2}).to eq(true)
-end
+     describe ' when block is given'do
+          it'should return true if any parameter meets the conditon'do
+          arr = [1,2,3,4,5]
+          expect(arr.my_any?{|ele| ele > 2}).to eq(true)
+          end
 
-it'should return false if any parametor meets the conditon'do
-arr = [1,2,3,4]
- expect(arr.my_any?{|ele| ele > 6}).to eq(false)
-end
-end
-describe' when parametor is given' do
-it 'should return true any of elements meet the condition' do
-arr = [2,3,5,6]
-expect(arr.my_any?(Integer)).to eq true
-end
+          it'should return false if any parameter meets the conditon'do
+          arr = [1,2,3,4]
+          expect(arr.my_any?{|ele| ele > 6}).to eq(false)
+          end
+     end
+     describe' when parameter is given' do
+          it 'should return true any of elements meet the condition' do
+          arr = [2,3,5,6]
+          expect(arr.my_any?(Integer)).to eq true
+          end
 
-it 'should return false any of elements do not meet the condition' do
-     arr = [2,3,5,6]
-     expect(arr.my_any?(String)).to eq false
+          it 'should return false any of elements do not meet the condition' do
+          arr = [2,3,5,6]
+          expect(arr.my_any?(String)).to eq false
+          end
+
      end
 
-end
-
-describe 'when parametor is not given' do
- it 'should return true if any elements are truthy' do
-     
- expect([1,2,3,"hie"].my_any?).to eq true
-end
-it 'should return false if all elements are falsy' do
-     
-     expect([nil,false].my_any?).to eq false
-    end
-end
+     describe 'when parameter is not given' do
+          it 'should return true if any elements are truthy' do
+               
+          expect([1,2,3,"hie"].my_any?).to eq true
+          end
+          it 'should return false if all elements are falsy' do
+               
+               expect([nil,false].my_any?).to eq false
+          end
+     end
 end
 
 describe "my_none?" do
      describe "when block is given" do
           it"returns true if none of the elements meet the condition" do
-               expect([1,1,1].my_none?{|el| el>2}).to eq(true)
+          expect([1,1,1].my_none?{|el| el>2}).to eq(true)
           end
-            it"returns false if any of the elements meet the condition" do
-               expect([1,1,3].my_none?{|el| el>2}).to eq(false)
-            end         
+          it"returns false if any of the elements meet the condition" do
+          expect([1,1,3].my_none?{|el| el>2}).to eq(false)
+          end         
      end
      describe "when param is given" do
           it "returns true if none of the elements meet the condition"  do
-               expect([true, "hey"].my_none?(Integer)).to eq(true)
+          expect([true, "hey"].my_none?(Integer)).to eq(true)
           end
           it "returns false if any of the elements meet the condition"  do
-               expect([1,2].my_none?(Integer)).to eq(false)
+          expect([1,2].my_none?(Integer)).to eq(false)
           end
      end
      describe 'should return false if any elements are truthy' do
@@ -171,5 +173,32 @@ describe "my_none?" do
           it 'should return true if none elements are truthy' do
           expect([nil , false].my_none?).to eq true
           end
+     end
+end
+
+describe"my_count" do
+     describe"when block is given" do
+     it 'should return the count of the elements in block matching condition' do
+          expect([1,2,3].my_count{|el|el> 2}).to eq(1)
+     end
+     it 'should return the count 0 if no elements in block match the condition' do
+          expect([1,2,3].my_count{|el|el> 3}).to eq(0)
+     end
+     end
+     describe"when parameter is given" do
+     it 'should return the count of the elements in block matching param condition' do
+          expect([1,2,3,"el"].my_count(Integer)).to eq(3)
+     end
+     it 'should return the count 0 if no elements in block match the param condition' do
+          expect([false, nil].my_count(Integer)).to eq(0)
+     end
+     end
+     describe "when param is not given"do
+      it "returns arr length if param is not given " do
+           expect([1,2,3].my_count).to eq(3)
+      end
+       it 'returns 0 for empty enumerables' do
+        expect([].my_count).to eq(0)
+      end
      end
 end
